e7185670316351170d23e9ef9d0d0a18c4721238
Enes Bal <ce.enesbal@gmail.com>
2017-11-13 19:38:25 +0300

    no message

---
+++ SimpleGui/DisplayTextInARectangle.java
@@ -0,0 +1,36 @@
+package SimpleGui;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by enes on 11/13/2017.
+ */
+public class DisplayTextInARectangle extends JPanel {
+    public void paint(Graphics g){
+        g.setFont(new Font("",0,100));
+        FontMetrics fm = getFontMetrics(new Font("",0,100));
+        String s = "message";
+        int x=5;
+        int y=5;
+
+        for(int i=0; i<s.length();i++){
+            char c = s.charAt(i);
+            int h = fm.getHeight();
+            int w = fm.charWidth(c);
+
+            g.drawRect(x,y,w,h);
+            g.drawString(String.valueOf(c),x,y+h);
+            x=x+w;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(new DisplayTextInARectangle());
+        frame.setSize(500,700);
+        frame.setVisible(true);
+    }
+}
