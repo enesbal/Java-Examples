0cd3e32c5cdc9cb63ca03d552b6b3e5a66c37d39
Enes Bal <ce.enesbal@gmail.com>
2017-12-18 14:47:00 +0300

    Create DisplayAllRunningThread.java

---
+++ Threading/DisplayAllRunningThread.java
@@ -0,0 +1,13 @@
+public class Main extends Thread {
+   public static void main(String[] args) {
+      Main t1 = new Main();
+      t1.setName("thread1");
+      t1.start();
+      ThreadGroup currentGroup = Thread.currentThread().getThreadGroup();
+      int noThreads = currentGroup.activeCount();
+      Thread[] lstThreads = new Thread[noThreads];
+      currentGroup.enumerate(lstThreads);
+      
+      for (int i = 0; i < noThreads; i++) System.out.println("Thread No:" + i + " = " + lstThreads[i].getName());
+   }
+}
